26b4adb247ec897b19d30394273b96c4a0053bcc
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-10-11 18:18:58 +0400

    upload

---
+++ info.js
@@ -0,0 +1,5 @@
+/*
+
+https://youtu.be/iP_HqoCuRI0
+25:08/2:30:50
+*/
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Navbar } from './components';
+
+function App() {
+  return (
+    // BEM 
+    <div className="app">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
