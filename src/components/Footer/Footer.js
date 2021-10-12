b8759c3987ed72f68bb2e492a89cbb2242d3f7f5
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-10-12 16:21:03 +0400

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Button } from '../../globalStyles'; 
 import {
     FooterContainer,
     FooterSubscription,
     FooterSubHeading,
-    FooterSubText
+    FooterSubText,
+    Form,
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinksItems,
+    FooterLinkTitle,
+    FooterLink
 } from './Footer.elements';
-
 
 const Footer = () => {
     return (
-        <div>
-            Footer Text
-        </div>>
-        {/*<FooterContainer>
+        <FooterContainer>
             <FooterSubscription>
                 <FooterSubHeading>
                     Join our exclusive membership to
@@ -21,9 +25,79 @@ const Footer = () => {
                 <FooterSubText>
                     You can unsubscribe at any time
                 </FooterSubText>
+                <Form>
+                    <FormInput 
+                        name="emai"
+                        type="email"
+                        placeholder="Your email"
+                    />
+                    <Button fontBig>
+                        Subscribe
+                    </Button>
+                </Form>
             </FooterSubscription>
-        </FooterContainer>/*/}
-        
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to="/">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Term of Services</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                        <FooterLink to="/">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Term of Services</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to="/">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Term of Services</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to="/">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Term of Services</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to="/">
+                        <SocialIcon/>
+                        ULTRA
+                    </SocialLogo>
+                    <WebsiteRights>
+                        ULTRA 2021
+                    </WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href="/" targer="_blank" aria-label="Facebook">
+                            <FaFacebook/>
+                        </SocialIconLink>
+                        <SocialIconLink href="/" targer="_blank" aria-label="Instagram">
+                            <FaInstagram/>
+                        </SocialIconLink>
+                        <SocialIconLink href="/" targer="_blank" aria-label="Youtube">
+                            <FaYoutube/>
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
+        </FooterContainer>
     )
 }
-export default FooterContainer
+export default Footer
